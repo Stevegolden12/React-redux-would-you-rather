9e0d784a2ab108c3db68681cff1886e611d41e0a
Steve Golden <sgolden2525@outlook.com>
2019-12-23 06:44:20 -0600

    setting up react to run
    Added node packages
    Added src folder and files
    index.js,
    index.css
    Added components folder and App.js file

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+//import { BrowserRouter as Router, Route } from 'react-router-dom' 
+//import { connect } from 'react-redux'
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>Home Testing</div>
+    )
+  }
+}
+
+export default App
