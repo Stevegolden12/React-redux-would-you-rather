14955eb92c19b2788aab6800100a0f6d7ab0cef6
Steve Golden <sgolden2525@outlook.com>
2019-12-26 08:30:49 -0600

    Added user and question actions
    Added user action
    
    Added question actions

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,6 +1,11 @@
+import { _getUsers } from '../utils/_DATA.js'
+
 export const RECEIVE_USER_IMAGE = 'RECEIVE_USER_IMAGE'
 
 export function receiveUserImage({ avatarURL }) {
   type: RECEIVE_USER_IMAGE,
   avatarURL,
 }
+
+
+
