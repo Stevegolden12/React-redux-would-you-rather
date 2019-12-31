6d2a3e76bdd72a08ca0f424976cdeb08477432f5
Steve Golden <sgolden2525@outlook.com>
2019-12-31 09:37:39 -0600

    using state to Login (In Process)
    In process of accessing state for Login

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,20 +2,20 @@ import { getInitialData } from '../utils/api';
 import { receiveUsers } from '../actions/users';
 import { receiveQuestions } from '../actions/questions';
 import { setAuthedUser } from '../actions/authUser';
-//import { showLoading, hideLoading } from 'react-redux-loading'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 /*                          Going to need to change AUTHED_ID                       */
 const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData() {
   return (dispatch) => {
-//    dispatch(showLoading())
+    dispatch(showLoading())
     return getInitialData()
       .then(({ users, questions }) => {
         dispatch(receiveUsers(users))
         dispatch(receiveQuestions(questions))
         dispatch(setAuthedUser(AUTHED_ID))
-//        dispatch(hideLoading())
+        dispatch(hideLoading())
       })
   }
 }
